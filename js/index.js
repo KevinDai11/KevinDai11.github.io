e26645e9d14e3b2e18f9f30272aaf68f091b9dce
KevinDai11 <71230236+KevinDai11@users.noreply.github.com>
2022-01-04 21:41:36 -0600

    js for switch toggle
    cancels animation

--- js/index.js
+++ js/index.js
@@ -20,3 +20,28 @@ navLinkF.addEventListener('click', () =>{
 if(document.getElementById("test")!=null){
     document.documentElement.style.setProperty('--fall','100vh');
 }
+
+
+const animationToggle = document.querySelector('switch');
+
+document.addEventListener('DOMContentLoaded', function () {
+    var checkbox = document.querySelector('input[type="checkbox"]');
+
+    checkbox.addEventListener('change', function () {
+        if (checkbox.checked) {
+            document.documentElement.style.setProperty('--aTail','None');
+            document.documentElement.style.setProperty('--aFalling','None');
+            document.documentElement.style.setProperty('--aShining','None');
+            document.documentElement.style.setProperty('--height1','0px');
+            document.documentElement.style.setProperty('--height2','0px');
+        }
+        else {
+            document.documentElement.style.setProperty('--aTail','tail 3s ease-in-out infinite');
+            document.documentElement.style.setProperty('--aFalling','falling 3s ease-in-out infinite');
+            document.documentElement.style.setProperty('--aShining','shining 3s ease-in-out infinite');
+            document.documentElement.style.setProperty('--height1','100px');
+            document.documentElement.style.setProperty('--height2','25px');
+
+        }
+    });
+  });
